fab3464b31d979d8161afa5c23a718a10fc5ec3d
Vladimir <70281965+kalinichew@users.noreply.github.com>
2021-06-14 11:23:52 +0300

    edit listing code

--- script.js
+++ script.js
@@ -2,77 +2,79 @@
 function onPageLoaded() {
     const input = document.querySelector("input");
     const divCard = document.querySelector("div.card_container");
-    
+
     function createTodo() {
-        
+
         // Карточка
         const mainCard = document.createElement("div");
-        const contentCard = document.createElement("span");
-        const btnContainer = document.createElement("div");
-        const btnEdit = document.createElement("a");
-        const checkBox = document.createElement('input');
-
-        
-        btnContainer.classList.add("btn_Container");
         mainCard.classList.add("card");
-        contentCard.classList.add("card__content");
+
+        //Контейнер для кнопки закрыть карточку и чек бокса
+        const btnContainer = document.createElement("div");
+        btnContainer.classList.add("btn_Container");
+
+        //Кнопка редактировать карточку
+        const btnEdit = document.createElement("a");
         btnEdit.classList.add("btn_edit");
+        btnEdit.append('Редактировать');
+
+        //Чекбокс
+        const checkBox = document.createElement('input');
         checkBox.classList.add("check")
         checkBox.type = "checkbox";
         checkBox.checked;
-
-
-        btnEdit.append('Редактировать');
-        let inputData = input.value;
-        contentCard.append(inputData);
 
         //Кнопка удалить
         const closeBtn = document.createElement('div');
         closeBtn.classList.add("btn_close")
 
-        
-        btnContainer.appendChild(checkBox);
-        mainCard.appendChild(btnContainer);
-        mainCard.appendChild(btnEdit);
-        btnContainer.appendChild(closeBtn);
-        
-
-        //Добавление карточки
-        divCard.appendChild(mainCard).append(contentCard);
+        //Создание области для записи и считываение из инпута
+        const contentCard = document.createElement("span");
+        contentCard.classList.add("card__content");
+        let inputData = input.value;
+        contentCard.append(inputData);
 
         //Обнуление поле инпут
         input.value = "";
 
-        //Передача функции слушателя для удаления карточки
-        listenDeleteTodo(closeBtn);
+        //Цепочка добавления чекбокса, кнопки редактировать и закрыть карточку
+        btnContainer.appendChild(checkBox);
+        mainCard.appendChild(btnContainer);
+        mainCard.appendChild(btnEdit);
+        btnContainer.appendChild(closeBtn);
 
-        //Слушанье карточки на клик
-        function modalLisen(item){
+
+        //Добавление карточки
+        divCard.appendChild(mainCard).append(contentCard);
+
+        //Слушанье кнопки редактировать на клик
+        function modalLisen(item) {
             item.addEventListener("click", modalShow);
         }
 
         //Слушанье крестика на закрытие
         function listenDeleteTodo(element) {
-            element.addEventListener("click", (event) => {
+            element.addEventListener("click", () => {
                 mainCard.remove();
-                event.stopPropagation();
             });
         }
 
-        modalLisen(btnEdit)
         //Запуск функции модального окна
+        modalLisen(btnEdit)
 
+        //Передача функции слушателя для удаления карточки
+        listenDeleteTodo(closeBtn);
 
-        function modalShow(){
+        function modalShow() {
 
-        let modalContainer = document.createElement('div'),
-            modalClose = document.createElement('div'),
-            modalContent = document.createElement('div'),
-            modalText = document.createElement('textarea'),
-            modalOk = document.createElement('div'),
-            modalCancel = document.createElement('div'),
-            controlBtn = document.createElement('div');
-            
+            let modalContainer = document.createElement('div'),
+                modalClose = document.createElement('div'),
+                modalContent = document.createElement('div'),
+                modalText = document.createElement('textarea'),
+                modalOk = document.createElement('div'),
+                modalCancel = document.createElement('div'),
+                controlBtn = document.createElement('div');
+
             modalContainer.classList.add('modal-container');
             modalClose.classList.add('close');
             modalContent.classList.add('modal-content');
@@ -96,49 +98,49 @@ function onPageLoaded() {
             modalOk.append('Сохранить');
             modalCancel.append('Отмена');
 
+            //Получает значение из карточки
             modalText.value = inputData;
 
-            //Отключение прокручивания основного окна
+            //Переменная pageYOffset запоминает на сколько вниз опустился скролл
             let isModalOpen = false;
             let pageYOffset = 0;
-            let openModal = function() {
+            let openModal = function () {
                 pageYOffset = window.pageYOffset;
                 modalContainer.classList.add('is-open');
                 isModalOpen = true;
             }
 
             //Функция удаления модального окна при закрытии
-            let closesModal = function() {
+            let closesModal = function () {
                 modalContainer.remove();
                 isModalOpen = false;
-              }
-    
-            
-              let onScroll = function(item) {
-                if (isModalOpen) {
-                  item.preventDefault();
-                  window.scrollTo(0, pageYOffset);
-                }
-              }
+            }
 
-            //Функция редактирования  
-              let editModal = function(){
-                contentCard.innerHTML= modalText.value;
+            //Функция слушает скролл при запущенном модальном окне, если он проиходит то в 
+            //скроллТо передаются четкое значение по осиY которое запоминалось ранее
+            let onScroll = function () {
+                if (isModalOpen) {
+                    window.scrollTo(0, pageYOffset);
+                }
+            }
+            document.addEventListener('scroll', onScroll);
+
+            //Функция редактирования  (передаем в карточку новое значение)
+            let editModal = function () {
+                contentCard.innerHTML = modalText.value;
                 inputData = modalText.value;
                 closesModal();
-              }
+            }
 
-              modalCancel.addEventListener('click', closesModal);
-              modalOk.addEventListener('click', editModal);
-              modalClose.addEventListener('click', closesModal);
-              openModal();
+            modalCancel.addEventListener('click', closesModal);
+            modalOk.addEventListener('click', editModal);
+            modalClose.addEventListener('click', closesModal);
+
+            //Запуск функции всегда для запоминания значений скролла по Y
+            openModal();
 
         }
-
-
-
     }
-
 
     //Создание карточки
     document.getElementById('btnInput').onclick = () => {
@@ -147,11 +149,11 @@ function onPageLoaded() {
 
     //Слушатель на создание карточки через enter
     input.addEventListener("keydown", (keyPressed) => {
-            if (keyPressed.key == 'Enter') {
-                createTodo();
-            }
+        if (keyPressed.key == 'Enter') {
+            createTodo();
+        }
     });
-   
+
 }
 
 
